e3050dc31e78067eaae38524a4ff1bcb04484b79
undefined <undefined.@undefineddeMBP.local>
2017-11-03 03:37:53 +0800

    Add Invoke-DnsServerChange.ps1
    Committer: 54Pany <54pany@gmail.com>

---
+++ Invoke-DnsServerChange.ps1
@@ -0,0 +1,33 @@
+function Invoke-DnsServerChange {
+<#
+.SYNOPSIS
+
+     -Name           Network Interface Name.
+     -Dns            DNS Server IP Addr.
+                
+.DESCRIPTION
+
+    Author: p4ny (@LESSNET)
+    License: BSD 3-Clause
+    Required Dependencies: None
+    Optional Dependencies: None
+
+.EXAMPLE
+    Change DNS Server
+
+    C:\PS> Invoke-DnsServerChange -Name 'Ethernet0' -Dns "119.29.29.29,114,114,114,114"
+#>
+
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]$Name,
+        [Parameter(Mandatory = $True)]
+        [string[]]$Dns
+    )
+    $Dns = $Dns.Split(',')
+    $InterfaceGuid = Get-NetAdapter -name $Name | Select -ExpandProperty 'InterfaceGuid'
+    $Guid = Get-WmiObject win32_networkadapterconfiguration -filter "SettingID = '$InterfaceGuid'"
+    $Guid.SetDNSServerSearchOrder($Dns) | Out-Null
+
+    echo "DNS: $Dns changed successfully!! "
+}
